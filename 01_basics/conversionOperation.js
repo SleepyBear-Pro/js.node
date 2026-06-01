35d7b60a08ef6ed7626ae6d681877a1485ddab05
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-01 10:14:11 +0000

    Operations

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,54 @@
+let score = 33;
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let loggedIn = "Ojas"
+
+let booleanIsloggedIn = Boolean(loggedIn);
+console.log(booleanIsloggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Ojas" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
+// ******************** Operations ***********************
+
+let value = 3
+
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = "Ojas"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + 2 + "3" + 4 + 5);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
