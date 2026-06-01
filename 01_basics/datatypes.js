19d6c04a2e0d9c4b08a250421d5e4d3828def8be
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-01 09:13:16 +0000

    DataTypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code in a new version
+
+// alert(3 + 3)  // we are using nodejs not browser
+
+// Datatypes
+// number => 2^53
+// string =>""
+// boolean => true/false
+// null +> standalone   
+// undefined
+// bigint
+// symbol => unique
+
+
+// object
+
+console.log(typeof null)  // object
