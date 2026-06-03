9630aa1e0eef2171a0897ef83ad0718ab61e3f64
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-03 13:17:34 +0000

    Scope

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -4,19 +4,58 @@ let a = 300
 if(true) {
     let a = 10
     const b = 20
-    console.log(a);
+    //console.log(a);
     
 }
 
-for(let i = 0; i < Array.length; i++) {
-    const element = array[i]
-}
+// for(let i = 0; i < Array.length; i++) {
+//     const element = array[i]
+// }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
 
+function one() {
+    const username = "Sleepy"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    //two()
+    
+}
+one()
+
+if(true) {
+    const username = "Sleepy"
+    if(username === "Sleepy") {
+        const website = " Youtube"
+        //console.log(username + website);
+        
+    }
+    //console.log(website);
+    
+}
+
+//console.log(username);
+
+// *******************************interesting******************************************
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+console.log(addone(5));
 
 
+//console.log(addtwo(4));
 
+const addtwo = function(num){
+    return num + 2
+}
 
+console.log(addtwo(4));
