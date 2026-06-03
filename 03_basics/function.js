b7f8b0e2717204f8488ef22d3179294015877024
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-03 11:28:12 +0000

    Function Done

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -29,7 +29,39 @@ function loginUsermessage(username = "Sleepy") {
     return `${username} just logged in`
 }
 
-console.log(loginUsermessage())
+//console.log(loginUsermessage())
+
+function calculateCartprice(val1 , val2 , ...num1) {
+    return num1
+}
+
+console.log(calculateCartprice(200,400,500,2000));
+
+const user = {
+    username : "Sleepy",
+    price : 999
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user)
+
+handleObject({
+    username: "Ojas",
+    price : 399
+})
+
+const myNewArray = [200, 300, 400, 100]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,600]));
+
 
 
 
