3120728efbb851f4f8f73919b37a975c78f52f50
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-04 03:13:09 +0000

    Arrow

---
+++ 03_basics/arrow.js
@@ -0,0 +1,49 @@
+const user = {
+    username : "Sleepy",
+    price : 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`)
+        console.log(this);
+        
+    }
+
+
+}
+
+user.welcomeMessage()
+user.username = "Sam"
+user.welcomeMessage()
+
+console.log(this);
+
+function chai() {
+    username: "Sleepy"
+    console.log(this);  
+    console.log(this.username);
+    
+}
+
+
+const chaiii = () => {
+    let username = "Sleepy"
+    console.log(this);
+    
+}
+
+chai()
+
+const addTwoo = (num1 , num2) =>  (num1 + num2)
+
+const addTwo = (num1 , num2) => ({username : "Sleepy"})
+
+console.log(addTwo());
+
+const array = [1,2,3,4,5,6]
+
+array.forEach(() => (0))
+
+
+
+
+
