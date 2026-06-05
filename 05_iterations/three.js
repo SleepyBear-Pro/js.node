6d8910724660c7007f7c07727477f72b3e5239d7
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-05 07:15:27 +0000

    For of Loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,34 @@
+// for of
+
+const arr = [1,2,3,4]
+
+for (const num of arr) {
+    console.log(`Value of num : ${num}`);
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United State of America")
+map.set('fr', "France")
+map.set('IN', "INDIA")
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject = {
+    'game1' : "NFS",
+    'game2' : "Spiderman"
+}
+
+for (const [key, value] of myObject) {
+    console.log(key , ':-', value);
+    
+}
