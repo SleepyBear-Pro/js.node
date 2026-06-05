d6b5553f4897be227ead5b9b08af927aef47ab7f
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-05 07:46:24 +0000

    For Each Loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,44 @@
+const coding = ["js", "rubby", "java", "python", "cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printme(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printme)
+
+// coding.forEach( (item,index,array) => {
+//     console.log(item, index, array);  
+// })
+
+const myCoding = [ 
+    {
+        languageName : "JavaScript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "Java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "Python",
+        languageFileName : "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+})
+
+
+
+
