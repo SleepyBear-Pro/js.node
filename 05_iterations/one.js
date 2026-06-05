ff9ff9b37d6a1578eb57aaf747856ffcd5c1c4cd
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-05 06:53:40 +0000

    Loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,44 @@
+//for
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if(element == 5) {
+        console.log("5 is the best number");
+        
+    }
+    console.log(element);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(`outer loop : ${i}`);
+    console.log();
+    for (let j = 0; j < 10; j++) {
+        console.log(`inner loop : ${j}`);
+        console.log(i + '*' + j + ' = ' + i*j );
+    }
+    console.log();   
+}
+
+let myarray = ["flash", "superman" , "batman"]
+console.log(myarray.length);
+
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element);
+}
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if(index == (5 , 4 , 3)) {
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(`Value of i is ${index}`);   
+}
+
+
+
+
+
+
