fcfce2b3eab05c0caecec064ecf3c2fd3f6220da
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-05 04:26:08 +0000

    if else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,3 +1,51 @@
+// if 
+const users = true
+
+const temperature = 41
+
+if(temperature < 50) {
+    console.log("less than 50");
+} else {
+    console.log("greater than 50");   
+}
+
+// < , > , <= , >= , == , != , === , !==
+const score = 200
+if(score > 100) {
+    const user =  "fly"
+    console.log(`User can : ${user}`);
+}
+
+// console.log(`User can : ${user}`);
+
+const balance = 1000
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if(balance < 750) {
+    console.log("less than 750");
+} else if(balance < 900) {
+    console.log("less than 900");
+} else {
+    console.log("Sufficient");
+    
+}
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+
+if(userLoggedIn && debitcard) {  // AND = &&
+    console.log("Verified");
+    
+}
+
+if (userLoggedIn || loggedInFromGoogle) {   // OR = ||
+    console.log("verified");
+    
+}
+
+
 
 
 
