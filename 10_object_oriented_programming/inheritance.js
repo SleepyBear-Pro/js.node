7b54149295d6875d443fbe2eb886b07e377fe8ac
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-08 23:16:03 +0530

    Inheritance , Static and Class

---
+++ 10_object_oriented_programming/inheritance.js
@@ -0,0 +1,41 @@
+class User {
+    constructor(username) {
+        this.username = username
+    }
+
+    logMe() {
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User {
+    constructor(username , email , password) {
+        super(username)
+        this.password = password
+        this.email = email
+    }
+
+    addCourse () {
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+chai.addCourse()
+chai.logMe()
+
+
+const masalaChai = new User("MasalaChai")
+masalaChai.logMe()
+
+console.log(chai === masalaChai);
+console.log(chai === Teacher);
+console.log(chai instanceof Teacher);
+console.log(chai instanceof User);
+
+
+
+
+
+
